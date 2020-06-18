a48d72a2c011ab0b1bdc531469c2f3652de61327
karudo <karudokun@gmail.com>
2020-06-18 21:19:47 +0100

    added login route

--- schema/resolvers/Query.js
+++ schema/resolvers/Query.js
@@ -1,11 +1,31 @@
 const User = require('../../models/User');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 module.exports = {
     user: async (parent, args) => {
-        
         const user = await User.findById(args.id);
         return user;
-        
     },
     users: async () => await User.find({}),
+    login: async (parent, args) => {
+
+        const { usernameOrEmail, password } = args;
+        const user = await User.findOne({ $or: [{username: usernameOrEmail}, {email: usernameOrEmail}] });
+
+        if (!user)
+            throw new Error("email or password is incorrect");
+
+        const passMatch = await bcrypt.compare(password, user.password);
+        if (!passMatch)
+            throw new Error("email or password is incorrect");
+
+        const { JWT_SECRET } = process.env;
+        const token = jwt.sign({ userId: user.id, email: user.email}, JWT_SECRET, {
+            expiresIn: '1h'
+        });
+
+        return { id: user.id, token, tokenExpiration: 1 }
+
+    }
 }

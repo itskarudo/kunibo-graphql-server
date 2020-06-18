bec7605ed7a3f7e3cf7374f73b8abdb9d5e7aae5
karudo <karudokun@gmail.com>
2020-06-18 21:20:19 +0100

    Added is-auth middleware

---
+++ middlewares/is-auth.js
@@ -0,0 +1,33 @@
+const jwt = require('jsonwebtoken');
+
+module.exports = (req, res, next) => {
+    const authHeader = req.get('Authorization');
+    if (!authHeader) {
+        req.isAuth = false;
+        return next();
+    }
+    const token = authHeader.split(' ')[1]; // Bearer <token>
+    if (!token || token === "") {
+        req.isAuth = false;
+        return next();
+    }
+
+    const { JWT_SECRET } = process.env;
+    try {
+        const decodedToken = jwt.verify(token, JWT_SECRET);
+    } catch (e) {
+        req.isAuth = false;
+        return next();
+    }
+
+    if (!decodedToken) {
+        req.isAuth = false;
+        return next();
+    }
+
+    req.isAuth = true;
+    req.userId = decodedToken.userId;
+
+    next();
+
+}
--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const Schema = require('./schema/schema');
+const isAuth = require('./middlewares/is-auth');
 
 require('dotenv').config();
 require('./database/db');
 
 const app = express();
+
+app.use(isAuth);
 
 app.use(graphqlHTTP({
     schema: Schema,
